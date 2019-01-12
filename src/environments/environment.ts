726e2eb3e611530980dd6150ace44360e4f938e2
pevargasg <wearetamo@gmail.com>
2019-01-12 01:30:44 -0800

    1) web api firebase info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBXMDtDW6JjGSFHnPFYCy9onkppakFAbDI",
+    authDomain: "megachat-e414c.firebaseapp.com",
+    databaseURL: "https://megachat-e414c.firebaseio.com",
+    projectId: "megachat-e414c",
+    storageBucket: "megachat-e414c.appspot.com",
+    messagingSenderId: "732664988316"
+  }
 };
 
 /*
