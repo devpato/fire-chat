655fb8d37d4083ccab2f39972b881741a6be259f
pevargasg <wearetamo@gmail.com>
2019-01-12 01:40:55 -0800

    add angular firestore modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ChatComponent } from './chat/chat.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { ChatComponent } from "./chat/chat.component";
+import { HomeComponent } from "./home/home.component";
+import { environment } from "src/environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChatComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, ChatComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
