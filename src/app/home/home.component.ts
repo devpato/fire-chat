14d4f15a14836b7633a18a845b381454d8ca7c50
pevargasg <wearetamo@gmail.com>
2019-01-13 01:07:48 -0800

    home component done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
+import { ChatService } from "../chat.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  userChats$;
+  constructor(public auth: AuthService, public cs: ChatService) {}
 
   ngOnInit() {
+    this.userChats$ = this.cs.getUserChats();
   }
-
 }
