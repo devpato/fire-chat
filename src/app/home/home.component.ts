d3fb5d78ce5731fbf746261107936cc5eb99b416
pevargasg <wearetamo@gmail.com>
2019-01-13 23:39:47 -0800

    Home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../auth.service";
 import { ChatService } from "../chat.service";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
