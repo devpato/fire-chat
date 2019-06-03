39e8ce40202131ff15352fac82b355a09692e991
pevargasg <wearetamo@gmail.com>
2019-06-02 19:59:46 -0700

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBXMDtDW6JjGSFHnPFYCy9onkppakFAbDI",
+    authDomain: "megachat-e414c.firebaseapp.com",
+    databaseURL: "https://megachat-e414c.firebaseio.com",
+    projectId: "megachat-e414c",
+    storageBucket: "megachat-e414c.appspot.com",
+    messagingSenderId: "732664988316"
+  }
 };
